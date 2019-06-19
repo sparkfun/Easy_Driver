23cbbf4dfefad5a26cf73e62d0e0ef2a2035f70c
Ho Yun "Bobby <ho.chan@colorado.edu>
2019-06-19 12:07:32 -0600

    Update SparkFun_Easy_Driver_Basic_Demo.ino
    Updating code to reflect 1000 steps based on description and how programmers start counting from 0 based on the `if()` statement's logic.

--- Firmware/SparkFun_Easy_Driver_Basic_Demo/SparkFun_Easy_Driver_Basic_Demo.ino
+++ Firmware/SparkFun_Easy_Driver_Basic_Demo/SparkFun_Easy_Driver_Basic_Demo.ino
@@ -93,7 +93,7 @@ void StepForwardDefault()
 {
   Serial.println("Moving forward at default step mode.");
   digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
-  for(x= 1; x<1000; x++)  //Loop the forward stepping enough times for motion to be visible
+  for(x= 0; x<1000; x++)  //Loop the forward stepping enough times for motion to be visible
   {
     digitalWrite(stp,HIGH); //Trigger one step forward
     delay(1);
@@ -109,7 +109,7 @@ void ReverseStepDefault()
 {
   Serial.println("Moving in reverse at default step mode.");
   digitalWrite(dir, HIGH); //Pull direction pin high to move in "reverse"
-  for(x= 1; x<1000; x++)  //Loop the stepping enough times for motion to be visible
+  for(x= 0; x<1000; x++)  //Loop the stepping enough times for motion to be visible
   {
     digitalWrite(stp,HIGH); //Trigger one step
     delay(1);
@@ -127,7 +127,7 @@ void SmallStepMode()
   digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
   digitalWrite(MS1, HIGH); //Pull MS1, and MS2 high to set logic to 1/8th microstep resolution
   digitalWrite(MS2, HIGH);
-  for(x= 1; x<1000; x++)  //Loop the forward stepping enough times for motion to be visible
+  for(x= 0; x<1000; x++)  //Loop the forward stepping enough times for motion to be visible
   {
     digitalWrite(stp,HIGH); //Trigger one step forward
     delay(1);
@@ -155,7 +155,7 @@ void ForwardBackwardStep()
       digitalWrite(dir,HIGH);
     }
     
-    for(y=1; y<1000; y++)
+    for(y=0; y<1000; y++)
     {
       digitalWrite(stp,HIGH); //Trigger one step
       delay(1);
